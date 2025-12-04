11586418d30e42ccee1b91c29a6263e9e032051d
khushi0433 <fatimahbaloch917@gmail.com>
2025-12-04 20:35:56 +0500

    fix: Cannot find module error

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const { Server } = require("socket.io");
 
+require("dotenv").config();
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 const trafficRoutes = require("./routes/trafficRoutes");
+const { errorHandler } = require("./middlewares/errorHandler");
+
+connectDB();
 
 const app = express();
 const server = http.createServer(app);
@@ -16,33 +21,15 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/traffic", trafficRoutes);
-
-require("./sockets/trafficSockets")(io);
-
-connectDB();
-
-require("dotenv").config();
-const authRoutes = require("./routes/authRoutes");
-const adminRoutes = require("./routes/adminRoutes");
-const websiteRoutes = require("./routes/websiteRoutes");
-const { errorHandler } = require("./middlewares/errorHandler");
-
-app.use(cors());
-app.use(express.json());
-
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
-app.use("/api/website", websiteRoutes);
 app.use("/api/traffic", trafficRoutes);
 
 // Error handling middleware
 app.use(errorHandler);
 
 require("./sockets/trafficSockets")(io);
-
-connectDB();
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
